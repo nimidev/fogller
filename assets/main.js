3620e6b1bad10c1f913118ecc5f669666c0fe051
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2018-06-25 00:48:38 +0300

    Add metalanguages and menu info

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,17 @@ $(document).ready(function () {
   var menuIsShown = false;
   var $header = $('.header');
   var $hamburger = $header.find('.hamburger');
+  var $info = $('.info');
+  var $body = $('body');
 
   $('.photos').fullpage({
     sectionSelector: '.photos__section',
     slideSelector: '.photos__slide',
+    controlArrows: false,
     onLeave: function () {
       if (!menuIsShown) {
         $('.photos').addClass('photos_with-menu');
-        $header.addClass('header_visible');
+        $body.addClass('header-visible');
 
         setTimeout(function () {
           menuIsShown = true;
@@ -24,5 +27,14 @@ $(document).ready(function () {
 
   $hamburger.on('click', function () {
     $hamburger.toggleClass('is-active');
-  })
+    $body.toggleClass('is-info-open');
+  });
+
+  $body.on('click', function (e) {
+    var $target = $(e.target);
+
+    if ($target.closest($hamburger).length === 0 && $target.closest($info).length === 0) {
+      $body.removeClass('is-info-open');
+    }
+  });
 });
--- index_en.md
+++ index_en.md
@@ -1,4 +1,4 @@
 ---
 layout: default
-lang: iw
+lang: en
 ---
